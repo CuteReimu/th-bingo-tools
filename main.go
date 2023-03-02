1bee620fc8852f28bfc37ebc298c0b92311cb906
奇葩の灵梦 <415551921@qq.com>
2023-03-03 00:50:39 +0800

    实现th18 E难度一面一符

--- main.go
+++ main.go
@@ -1,9 +1,52 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/sys/windows"
+	"time"
+)
+
 func main() {
-	pid, err := getPidByProcessName("chrome.exe")
+	pid, err := getPidByProcessName("th18.exe")
 	if err != nil {
 		panic(err)
 	}
-	getProcessHandle(uint32(pid))
+	hand, err := getProcessHandle(uint32(pid))
+	if err != nil {
+		panic(err)
+	}
+	baseAddress, err := getModuleBaseAddress(hand, "th18.exe")
+	if err != nil {
+		panic(err)
+	}
+	spellPracticeGet, spellPracticeTotal, gameModeGet, gameModeTotal := getCount(hand, baseAddress)
+	fmt.Println("1面1符E难度 Game Mode: ", gameModeGet, "/", gameModeTotal)
+	fmt.Println("1面1符E难度 Spell Practice: ", spellPracticeGet, "/", spellPracticeTotal)
+	for {
+		spellPracticeGet2, spellPracticeTotal2, gameModeGet2, gameModeTotal2 := getCount(hand, baseAddress)
+		//fmt.Println("1面1符E难度 Game Mode: ", gameModeGet2, "/", gameModeTotal2)
+		//fmt.Println("1面1符E难度 Spell Practice: ", spellPracticeGet2, "/", spellPracticeTotal2)
+		if spellPracticeTotal2 > spellPracticeTotal {
+			fmt.Println("符卡开始 Spell Practice")
+		}
+		if spellPracticeGet2 > spellPracticeGet {
+			fmt.Println("符卡收取 Spell Practice")
+		}
+		if gameModeTotal2 > gameModeTotal {
+			fmt.Println("符卡开始 Game Mode")
+		}
+		if gameModeGet2 > gameModeGet {
+			fmt.Println("符卡收取 Game Mode")
+		}
+		spellPracticeGet, spellPracticeTotal, gameModeGet, gameModeTotal = spellPracticeGet2, spellPracticeTotal2, gameModeGet2, gameModeTotal2
+		time.Sleep(time.Second / 2)
+	}
+}
+
+func getCount(hand windows.Handle, baseAddress uintptr) (spellPracticeGet, spellPracticeTotal, gameModeGet, gameModeTotal uint32) {
+	_, gameModeGet = readMemory[uint32](hand, baseAddress, 0xCF41C, 0x998)
+	_, spellPracticeGet = readMemory[uint32](hand, baseAddress, 0xCF41C, 0x99C)
+	_, gameModeTotal = readMemory[uint32](hand, baseAddress, 0xCF41C, 0x9A0)
+	_, spellPracticeTotal = readMemory[uint32](hand, baseAddress, 0xCF41C, 0x9A4)
+	return
 }
--- process.go
+++ process.go
@@ -8,11 +8,12 @@ import (
 	"strconv"
 	"strings"
 	"syscall"
+	"unsafe"
 )
 
 // 根据进程名获取进程ID
-func getPidByProcessName(name string) (int64, error) {
-	task := exec.Command("cmd", "/c", "wmic", "process", "get", "name,", "ProcessId", "|", "findstr", name)
+func getPidByProcessName(processName string) (int64, error) {
+	task := exec.Command("cmd", "/c", "wmic", "process", "get", "name,", "ProcessId", "|", "findstr", processName)
 	data, _ := task.CombinedOutput()
 	res := strings.Split(string(data), "\n")[0] //取第一行程序结果
 	ss := regexp.MustCompile(`\s+`).Split(res, -1)
@@ -21,18 +22,49 @@ func getPidByProcessName(name string) (int64, error) {
 }
 
 // 获取进程的句柄
-func getProcessHandle(pid uint32) windows.Handle {
-	hand, err := windows.OpenProcess(syscall.STANDARD_RIGHTS_ALL|0xFFFF, false, pid)
-	if err != nil {
-		if err.Error() != "The operation completed successfully." {
-			log.Println("报错：", err.Error())
-			syscall.Exit(1)
+func getProcessHandle(pid uint32) (windows.Handle, error) {
+	return windows.OpenProcess(syscall.STANDARD_RIGHTS_ALL|0xFFFF, false, pid)
+}
+
+// 获取模块基地址
+func getModuleBaseAddress(hand windows.Handle, processName string) (uintptr, error) {
+	hModel := [10000]windows.Handle{0}
+	var num uint32
+	if err := windows.EnumProcessModules(hand, &hModel[0], uint32(len(hModel)), &num); err != nil {
+		return 0, err
+	}
+	for i := uint32(0); i < num; i++ {
+		tmp := [50]uint16{0}
+		if err := windows.GetModuleBaseName(hand, hModel[i], &tmp[0], uint32(len(tmp))); err != nil {
+			log.Println(err)
+			continue
+		}
+		sb := strings.Builder{}
+		for _, v := range tmp {
+			if v != 0 {
+				sb.WriteByte(byte(v))
+			}
+		}
+		if strings.EqualFold(processName, sb.String()) {
+			return uintptr(hModel[i]), nil
 		}
 	}
-	if hand <= 0 {
-		log.Println("打开句柄失败", hand)
-		syscall.Exit(1)
+	return 0, nil
+}
+
+// 通过基址+指针链读取到指针地址的值
+func readMemory[T any](handle windows.Handle, baseAddress uintptr, addrLinks ...uintptr) (uintptr, T) {
+	addr := baseAddress
+	for _, x := range addrLinks[:len(addrLinks)-1] {
+		var tmp64 uint32
+		addr += x
+		//fmt.Printf("地址%X\t", addr)
+		windows.ReadProcessMemory(handle, addr, (*byte)(unsafe.Pointer(&tmp64)), unsafe.Sizeof(tmp64), nil)
+		addr = uintptr(tmp64)
 	}
-	log.Println("[+] 打开目标进程成功", hand)
-	return hand
+	addr += addrLinks[len(addrLinks)-1]
+	//fmt.Printf("地址%X\n", addr)
+	var value T
+	windows.ReadProcessMemory(handle, addr, (*byte)(unsafe.Pointer(&value)), unsafe.Sizeof(value), nil)
+	return addr, value
 }
